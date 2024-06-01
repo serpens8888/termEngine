15e2ec495168041f4a50cd3a52bd4a04f32163cb
serpens8888 <serpens8888@github.com>
2024-06-01 08:48:08 -0400

    cool effect

--- v3/src/initialize.cpp
+++ v3/src/initialize.cpp
@@ -7,9 +7,9 @@
 void initialize(){
 	std::cout << "\033[?25l";
 	frame = pixels();
-	for(int i = 0;i<1000;i++){
-	Hline line(0,i%(cols),i/2,223+i,92+i,222+i);	
-	Vline line2(0,(i/2)%(rows),i,15+i,255+i,55+i);
+	for(int i = 0;i<1000;i+=2){
+	Hline line(0,i,i,0+i,0+i,0+i);	
+	Vline line2(0,i,i,123+i,123+i,123+i);
 	
 	std::cout << "\033[0;0H";
 	std::cout << render(frame);
